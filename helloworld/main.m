89200eb27c90dcc094f4b906fd33e5ac7d9d46f4
Akira Ito <akiraito108@gmail.com>
2014-01-29 02:38:00 +0900

    Initial Commit

---
+++ helloworld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by akira on 2014/01/29.
+//  Copyright (c) 2014年 akira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
